f086dc923b439b0163a0cb925b4554cd7f9dd90c
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-14 12:13:13 +0800

    Use focused operator screen as operations home

--- src/navigation/OperationsStack.tsx
+++ src/navigation/OperationsStack.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import OperationsScreen from "../screens/operations/OperationsScreen";
+import OperatorScreen from "../screens/operations/OperatorScreen";
 import OperationModuleScreen from "../screens/operations/OperationModuleScreen";
 import OperationListScreen from "../screens/operations/OperationListScreen";
 import OperationDetailScreen from "../screens/operations/OperationDetailScreen";
@@ -11,12 +11,12 @@ const Stack = createNativeStackNavigator();
 export default function OperationsStack() {
   return (
     <Stack.Navigator
-      initialRouteName="OperationsHome"
+      initialRouteName="OperatorHome"
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="OperationsHome" component={OperationsScreen} />
+      <Stack.Screen name="OperatorHome" component={OperatorScreen} />
       <Stack.Screen name="OperationModule" component={OperationModuleScreen} />
       <Stack.Screen name="OperationList" component={OperationListScreen} />
       <Stack.Screen name="OperationDetail" component={OperationDetailScreen} />
