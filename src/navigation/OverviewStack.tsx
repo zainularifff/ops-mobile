cc11619672999dfa8185381ef10076b486ef6f50
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-14 23:32:13 +0800

    Simplify overview endpoint navigation flow

--- src/navigation/OverviewStack.tsx
+++ src/navigation/OverviewStack.tsx
@@ -2,20 +2,12 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import OverviewHomeScreen from "../screens/overview/OverviewHomeScreen";
-
-import EndpointSummaryScreen from "../screens/endpoint/EndpointSummaryScreen";
-import ActiveDeviceCoverageScreen from "../screens/endpoint/ActiveDeviceCoverageScreen";
 import ActiveDeviceListScreen from "../screens/endpoint/ActiveDeviceListScreen";
-import EndpointIssueListScreen from "../screens/endpoint/EndpointIssueListScreen";
-import SiteEndpointSummaryScreen from "../screens/endpoint/SiteEndpointSummaryScreen";
 import DeviceQuickViewScreen from "../screens/endpoint/DeviceQuickViewScreen";
-
 import TicketSummaryScreen from "../screens/ticket/TicketSummaryScreen";
 import TicketWorkloadListScreen from "../screens/ticket/TicketWorkloadListScreen";
 import TicketQuickViewScreen from "../screens/ticket/TicketQuickViewScreen";
 
-import RiskSummaryScreen from "../screens/risk/RiskSummaryScreen";
-import ExceptionDetailScreen from "../screens/exception/ExceptionDetailScreen";
 const Stack = createNativeStackNavigator();
 
 export default function OverviewStack() {
@@ -27,17 +19,11 @@ export default function OverviewStack() {
       }}
     >
       <Stack.Screen name="OverviewHome" component={OverviewHomeScreen} />
-      <Stack.Screen name="EndpointSummary" component={EndpointSummaryScreen} />
-      <Stack.Screen name="ActiveDeviceCoverage" component={ActiveDeviceCoverageScreen} />
       <Stack.Screen name="ActiveDeviceList" component={ActiveDeviceListScreen} />
+      <Stack.Screen name="DeviceQuickView" component={DeviceQuickViewScreen} />
       <Stack.Screen name="TicketSummary" component={TicketSummaryScreen} />
       <Stack.Screen name="TicketWorkloadList" component={TicketWorkloadListScreen} />
       <Stack.Screen name="TicketQuickView" component={TicketQuickViewScreen} />
-      <Stack.Screen name="RiskSummary" component={RiskSummaryScreen} />
-      <Stack.Screen name="ExceptionDetail" component={ExceptionDetailScreen} />
-      <Stack.Screen name="EndpointIssueList" component={EndpointIssueListScreen} />
-      <Stack.Screen name="SiteEndpointSummary" component={SiteEndpointSummaryScreen} />
-      <Stack.Screen name="DeviceQuickView" component={DeviceQuickViewScreen} />
     </Stack.Navigator>
   );
 }
