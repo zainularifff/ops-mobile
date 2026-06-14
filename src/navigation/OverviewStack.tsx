843ae7e1e4609359ec760c502a88e9558ee78d6c
zainulariff <80114394+zainularifff@users.noreply.github.com>
2026-06-15 00:41:53 +0800

    Wire geolocation screens into overview stack

--- src/navigation/OverviewStack.tsx
+++ src/navigation/OverviewStack.tsx
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OverviewHomeScreen from "../screens/overview/OverviewHomeScreen";
 import ActiveDeviceListScreen from "../screens/endpoint/ActiveDeviceListScreen";
 import DeviceQuickViewScreen from "../screens/endpoint/DeviceQuickViewScreen";
+import GeolocationSummaryScreen from "../screens/geolocation/GeolocationSummaryScreen";
+import GeolocationHistoryScreen from "../screens/geolocation/GeolocationHistoryScreen";
 import TicketSummaryScreen from "../screens/ticket/TicketSummaryScreen";
 import TicketWorkloadListScreen from "../screens/ticket/TicketWorkloadListScreen";
 import TicketQuickViewScreen from "../screens/ticket/TicketQuickViewScreen";
@@ -21,6 +23,8 @@ export default function OverviewStack() {
       <Stack.Screen name="OverviewHome" component={OverviewHomeScreen} />
       <Stack.Screen name="ActiveDeviceList" component={ActiveDeviceListScreen} />
       <Stack.Screen name="DeviceQuickView" component={DeviceQuickViewScreen} />
+      <Stack.Screen name="GeolocationSummary" component={GeolocationSummaryScreen} />
+      <Stack.Screen name="GeolocationHistory" component={GeolocationHistoryScreen} />
       <Stack.Screen name="TicketSummary" component={TicketSummaryScreen} />
       <Stack.Screen name="TicketWorkloadList" component={TicketWorkloadListScreen} />
       <Stack.Screen name="TicketQuickView" component={TicketQuickViewScreen} />
